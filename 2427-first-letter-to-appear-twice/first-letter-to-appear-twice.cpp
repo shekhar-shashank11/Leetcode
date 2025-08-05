e5480b9ddbe22428e55f617dc329e17ce83478cb
Shashank Shekhar <130744962+shekhar-shashank11@users.noreply.github.com>
2025-08-05 10:25:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (87.47%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+       set<char>st;
+        for(int i=0;i<s.size();i++){
+            if(!st.empty()&& st.find(s[i])!=st.end()) {
+
+                return s[i];
+            }
+            else{
+                st.insert(s[i]);
+            }
+        }
+        return  ' ';
+
+        
+    }
+};
